44ca7f12409c0458c21fc3f90001e4d62afe8188
Eric <ericodejafidelis@gmail.com>
2026-06-07 15:26:32 +0100

    feat: implement global validation pipe

--- src/auth/dto/createUser.dto.ts
+++ src/auth/dto/createUser.dto.ts
@@ -1,8 +1,34 @@
-import { Role } from "../../generated/prisma/enums";
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Role } from '../../generated/prisma/enums';
 
 export class CreateUser {
-  name: string
-  email: string
-  password: string
-  role?: Role
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
+  password: string;
+
+  @IsOptional()
+  @IsEnum(Role)
+  role?: Role;
 }
