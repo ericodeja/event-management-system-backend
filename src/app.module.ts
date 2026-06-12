c742785251c3ce37637ca61115a1ac5a1de829cd
Eric <ericodejafidelis@gmail.com>
2026-06-13 00:55:53 +0100

    feat: implement role-based access control with custom guard, decorator, and ticket type DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './lib/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './common/guards/role.guard';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    PrismaService,
+    { provide: APP_GUARD, useClass: RolesGuard },
+  ],
 })
-export class AppModule { }
+export class AppModule {}
